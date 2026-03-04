6ba0fd7224c38c215a2a2a005d6c2c4141449179
S Faraz <syedfaraz2530@gmail.com>
2026-03-04 21:25:27 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,57 @@
+import streamlit as st
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+
+st.title("Rainfall Prediction System")
+
+# Load dataset
+data = pd.read_csv("weatherAUS.csv")
+
+# Preprocess
+data = data.dropna()
+data['RainTomorrow'] = data['RainTomorrow'].map({'Yes':1,'No':0})
+
+features = [
+'MinTemp','MaxTemp','Rainfall','WindGustSpeed',
+'Humidity9am','Humidity3pm','Pressure9am','Pressure3pm',
+'Temp9am','Temp3pm','WindSpeed9am','WindSpeed3pm'
+]
+
+X = data[features]
+y = data['RainTomorrow']
+
+# Train model
+model = RandomForestClassifier()
+model.fit(X,y)
+
+st.header("Enter Weather Details")
+
+MinTemp = st.number_input("Min Temperature")
+MaxTemp = st.number_input("Max Temperature")
+Rainfall = st.number_input("Rainfall")
+WindGustSpeed = st.number_input("Wind Gust Speed")
+Humidity9am = st.number_input("Humidity 9am")
+Humidity3pm = st.number_input("Humidity 3pm")
+Pressure9am = st.number_input("Pressure 9am")
+Pressure3pm = st.number_input("Pressure 3pm")
+Temp9am = st.number_input("Temp 9am")
+Temp3pm = st.number_input("Temp 3pm")
+WindSpeed9am = st.number_input("Wind Speed 9am")
+WindSpeed3pm = st.number_input("Wind Speed 3pm")
+
+if st.button("Predict"):
+
+    input_data = np.array([[MinTemp,MaxTemp,Rainfall,WindGustSpeed,
+                            Humidity9am,Humidity3pm,
+                            Pressure9am,Pressure3pm,
+                            Temp9am,Temp3pm,
+                            WindSpeed9am,WindSpeed3pm]])
+
+    prediction = model.predict(input_data)
+
+    if prediction[0] == 1:
+        st.success("Rain Tomorrow 🌧️")
+    else:
+        st.success("No Rain Tomorrow ☀️")
